e1bcb8d2d0abba56a6818d9bdafc42d9fa69f3ef
joeldotdias <joeldias0204@gmail.com>
2025-04-17 09:02:16 +0530

    re

--- src/database.rs
+++ src/database.rs
@@ -1,29 +1,26 @@
-use sqlx::{
-    mysql::MySqlPoolOptions,
-    Pool,
-    MySql
-};
+use sqlx::{mysql::MySqlPoolOptions, MySql, Pool};
 
 pub async fn db_connection(database_url: &str) -> Pool<MySql> {
     match MySqlPoolOptions::new()
         .max_connections(69)
         .connect(database_url)
-        .await {
-            Ok(pool) => {
-                println!("Data Be Based");
-                match sqlx::migrate!("./migrations").run(&pool).await {
-                    Ok(_) => {
-                        println!("Migrations performed successfully");
-                    },
-                    Err(err) => {
-                        println!("Couldn't perform migrations: {}", err);
-                    },
-                };
-                pool
-            }
-            Err(err) => {
-                dbg!("Data Be Soy: {}", err);
-                std::process::exit(1);
-            }
+        .await
+    {
+        Ok(pool) => {
+            println!("Data Be Based");
+            match sqlx::migrate!("./migrations").run(&pool).await {
+                Ok(_) => {
+                    println!("Migrations performed successfully");
+                }
+                Err(err) => {
+                    println!("Couldn't perform migrations: {}", err);
+                }
+            };
+            pool
+        }
+        Err(err) => {
+            dbg!("Data Be Soy: {}", err);
+            std::process::exit(1);
+        }
     }
 }
